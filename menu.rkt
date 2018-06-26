d5fe9dd36dd661e9fa84fe67205a776a5d49a7a3
adam <ibrahimadam193@gmail.com>
2018-06-26 17:14:12 -0400

    Create menu with big bang
    Changes to be committed:
          modified:   menu.rkt

--- menu.rkt
+++ menu.rkt
@@ -4,8 +4,13 @@
 ; the menu, and selecting a menu item means causing the value for that
 ; key to be the final state of the menu.
 
+(provide
+  menu? menu-title menu-vals menu-items
+  menu-next menu-prev menu-ref
+  draw-menu)
 (require 2htdp/image)
 (require 2htdp/universe)
+(require "component-state.rkt")
 
 (define rem 30)
 ; number -> integer
@@ -14,13 +19,21 @@
 (define (draw-menu-item item)
   (text item rem 'black))
 
+(module+ test
+  (draw-menu-item "item"))
+
 (define cursor
   (beside (rectangle (make-font-size (* 3/2 rem)) 
                      rem 'solid 'black)
           (rotate -90
                   (triangle rem 'solid 'black))))
 
-(define (draw-menu title items selected-item)
+(module+ test 
+  cursor
+  (beside cursor (draw-menu-item "item")))
+
+
+(define (draw-menu-items title items selected-item)
   (define (draw-iter items-left menu-so-far distance-to-selected)
     (if (null? items-left)
       menu-so-far
@@ -37,37 +50,43 @@
   (let [(initial-menu (text title (make-font-size (* 3/2 rem)) 'black))]
     (draw-iter items initial-menu selected-item)))
 
-;(define items (list "one" "two" "three"))
-(define vals (build-list 5 identity))
-(define items (map number->string vals))
-
-(define-struct menu [vals items])
+(define-struct menu [title vals items])
 ; Takes in index of currently selected item, returns index of next
 ; item.
 (define (menu-next menu index)
   (modulo (add1 index) (length (menu-items menu))))
 (define (menu-prev menu index)
   (modulo (sub1 index) (length (menu-items menu))))
-(define (menu-value menu index)
+(define (menu-ref menu index)
   (second (list-ref (menu-vals menu) index)))
-(define tower-menu (make-menu vals items))
 
-(big-bang
-  0
-  [to-draw 
-    (lambda (state)
-      (draw-menu "Select Tower Size:" 
-                 (menu-items tower-menu)
-                 state))]
-  [on-key
-    (lambda (state key)
-      (let [(return "\r")]
-        (case key
-          ; High parts of a menu are earlier in list
-          ; Low parts of a menu are later in list
-          [("down" "j") (menu-next tower-menu state)]
-          [("up" "k") (menu-prev tower-menu state)]
-          [("q" return) #f]
-          [else state])))]
-  [stop-when boolean?])
+(define (draw-menu menu selected-item)
+  (draw-menu-items (menu-title menu) 
+                   (menu-items menu)
+                   selected-item))
+
+
+(module+ test
+  (define vals (build-list 5 identity))
+  (define items (map number->string vals))
+  (define test-menu (make-menu "Select Tower Size:" vals items))
+
+  (big-bang
+    (make-state #t 0)
+    [to-draw 
+      (lambda (state)
+        (draw-menu test-menu (state-public state)))]
+    [on-key
+      (lambda (state key)
+        (let [(return "\r") (menu-entry (state-public state))]
+          (case key
+            ; High parts of a menu are earlier in list
+            ; Low parts of a menu are later in list
+            [("down" "j") 
+             (set-state-public state (menu-next test-menu menu-entry))]
+            [("up" "k")
+             (set-state-public state (menu-prev test-menu menu-entry))]
+            [("q" return) (set-state-private state #f)]
+            [else state])))]
+    [stop-when (lambda (state) (eq? #f (state-private state)))]))
   
