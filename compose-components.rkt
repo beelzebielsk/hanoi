6c70b51960792acb9afedb957cf21a1c59ffb785
adam <ibrahimadam193@gmail.com>
2018-07-09 03:00:35 -0400

    Add support for on-tick to compose-components
    Changes to be committed:
          modified:   compose-components.rkt

--- compose-components.rkt
+++ compose-components.rkt
@@ -18,7 +18,7 @@
 ; this initializer is, and this should be public knowledge; it's part
 ; of that component's interface.
 ;
-; So, a component is a procedure. The contract of a procedure, which
+; So, a component is a procedure. The contract of a component, which
 ; the procedure must adhere to is:
 ; The information that it takes in as an initializer, and the
 ; information that it "returns" as it's final state. Everything else
@@ -55,7 +55,7 @@
 ; - (compose-components 'test comp1 comp2 comp3): The initializer and
 ;   final state of this component is the initlaizer of comp1 and the
 ;   final state of comp3.
-(define (compose-components components)
+(define (compose-components . components)
   (lambda (initializer)
     (define screen-width 1000)
     (define screen-height 500)
@@ -71,6 +71,15 @@
         (/ screen-width 2) (/ screen-height 2)
         'middle 'middle
         base-scene))
+    ; Changes state from one game to the next. Used when the current
+    ; component's state would cause the stop-when? function of the
+    ; current component to be true.
+    ; - If there are no more components left, then the next state should
+    ; cause the stop-when? function of the composed component to be
+    ; true.
+    ; - Otherwise, the composed component should initialize the next
+    ;   component, and start using the to-draw, on-key, etc. functions
+    ;   of the next component.
     (define (transition state)
       (let* [(games-left 
                (control-state-components-left (state-private state)))
@@ -92,12 +101,29 @@
         (if ((current-game 'stop-when) next-game-state)
           (transition (set-state-public state next-game-state))
           (set-state-public state next-game-state))))
+    ; The on-tick function of the composition behaves as the identity
+    ; function if the current component does not expose an on-tick
+    ; function, and if the current component does expose an on-tick
+    ; function, then that function is used.
+    (define on-tick
+      (lambda (state)
+        (let [(current-game (get-current-game state))]
+          (if (void? (current-game 'on-tick))
+            state
+            (let* [(current-game-state (state-public state))
+                   (next-game-state 
+                     ((current-game 'on-tick) current-game-state))]
+              (if ((current-game 'stop-when) next-game-state)
+                (transition (set-state-public state next-game-state))
+                (set-state-public state next-game-state)))))))
+
     (define stop-when (compose1 boolean? state-private))
     (define output state-public)
     (lambda (dispatch)
       (case dispatch
         [(to-draw) to-draw]
         [(on-key) on-key]
+        [(on-tick) on-tick]
         [(output) output]
         [(stop-when) stop-when]
         [(initial-state) 
@@ -107,6 +133,7 @@
                (cdr components)
                first-game)
              (first-game 'initial-state)))]))))
+
 
 ; component? procedure? -> component?
 ; This takes in a component and a function and returns a new component.
@@ -162,3 +189,37 @@
     [to-draw (game1 'to-draw)]
     [on-key (game1 'on-key) ]
     [stop-when (game1 'stop-when)]))
+
+; An example of on-tick behavior
+(module+ test
+(define (seconds->ticks seconds) (* seconds 28))
+(define (ticks->seconds ticks) (/ ticks 28))
+(define (draw-box x-pos y-pos) 
+  (let [(box (rectangle 30 10 'solid 'black))
+        (scene (rectangle 400 300 'solid 'white))]
+    (place-image box x-pos y-pos scene)))
+(define (draw ticks) (draw-box ticks ticks))
+(define (silly-box initializer)
+  (lambda (dispatch)
+    (case dispatch
+      [(to-draw) draw]
+      [(on-tick) add1]
+      [(initial-state) 0]
+      [(output) identity]
+      [(stop-when) (lambda (state) (>= (ticks->seconds state) 1))])))
+
+  (define game3
+    ((compose-components 
+       (make-output-adaptor 
+         silly-box 
+         (lambda (output)
+           (list "Choose Tower Size:"
+                 menu-items
+                 tower-heights)))
+       make-menu-game make-towers-game) (void)))
+  (big-bang
+    (game3 'initial-state)
+    [to-draw (game 'to-draw)]
+    [on-key (game 'on-key)]
+    [on-tick (game 'on-tick)]
+    [stop-when (game 'stop-when)]))
